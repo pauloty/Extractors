a8b3a85368a373140237496332b16bf882b7fbca
Paulo <paulorcb2011@hotmail.com>
2018-12-12 19:37:58 -0200

    Split in functions, merging all images into one CSV

--- Extractor.py
+++ Extractor.py
@@ -9,44 +9,56 @@ import os
 from scipy.stats import kurtosis, skew
 
 
-def extractors(path):
-    # Descoberta de todas as imagens no diretorio passado
-    imdir = 'output\\' + path + '\\'
-    print(imdir)
-    specifier = '_from_' + path  # Especificador para cada .csv
-    ext = ['png', 'jpg']
-    files = []
-    [files.extend(glob.glob(imdir + '*.' + e)) for e in ext]
-    images = [cv2.imread(file, cv2.IMREAD_GRAYSCALE) for file in files]
-
+def statisticalMoments(files, images):
     # Codigo para momentos estatisticos
-    myFile = open('CSVs\\StatiscalMoments' + specifier + '.csv', 'w', newline="")
+
     csv.register_dialect('dial', delimiter=';')
+    myFile = open('CSVs\\StatiscalMoments_High.csv', 'w', newline="") # High file
     writer = csv.writer(myFile, dialect='dial')
+    myFile1 = open('CSVs\\StatiscalMoments_Low.csv', 'w', newline="") # Low file
+    writer1 = csv.writer(myFile1, dialect='dial')
+    myFile2 = open('CSVs\\StatiscalMoments_Severe.csv', 'w', newline="") # Lesao Grave file
+    writer2 = csv.writer(myFile2, dialect='dial')
+    myFile3 = open('CSVs\\StatiscalMoments_Normal.csv', 'w', newline="") # Lesao Normal file
+    writer3 = csv.writer(myFile3, dialect='dial')
+
     row = []
     i = 0
     for img in images:
         row.append(files[i].rsplit('\\', 1)[1])   # Adiciona o nome da imagem no arquivo
-        # row.append(files[i])                    # Adiciona o caminho da imagem no arquivo
         row.append(np.mean(img))
         row.append(np.var(img))
-        # row.extend(list(skew(img)))  // axis = 0
-        # row.extend(list(kurtosis(img))) // axis = 0
         row.append(kurtosis(img, axis=None, fisher=False))  # Nao usa a definicao de Fisher e calcula sobre a imagem inteira
         row.append(skew(img, axis=None))
-        if 'suspect' in files[i]:
-            row.append(1)
-        else:
-            row.append(2)
-        writer.writerow(row)
+        if 'high' in files[i]:
+            writer.writerow(row)
+        if 'low' in files[i]:
+            writer1.writerow(row)
+        if 'severe' in files[i]:
+            writer2.writerow(row)
+        if 'normal' in files[i]:
+            writer3.writerow(row)
         row[:] = []
         i = i + 1
-    myFile.close()
 
-    # CODIGO MOMENTOS DE HU
-    myFile = open('CSVs\\HuMoments' + specifier + '.csv', 'w', newline="")
+    myFile.close()
+    myFile1.close()
+    myFile2.close()
+    myFile3.close()
+
+
+def huMoments(files, images):
+
     csv.register_dialect('dial', delimiter=';')
+    myFile = open('CSVs\\HuMoments_High.csv', 'w', newline="") # High file
     writer = csv.writer(myFile, dialect='dial')
+    myFile1 = open('CSVs\\HuMoments_Low.csv', 'w', newline="") # Low file
+    writer1 = csv.writer(myFile1, dialect='dial')
+    myFile2 = open('CSVs\\HuMoments_Severe.csv', 'w', newline="") # Lesao Grave file
+    writer2 = csv.writer(myFile2, dialect='dial')
+    myFile3 = open('CSVs\\HuMoments_Normal.csv', 'w', newline="") # Lesao Normal file
+    writer3 = csv.writer(myFile3, dialect='dial')
+
     row = []
     i = 0
     for img in images:
@@ -54,53 +66,75 @@ def extractors(path):
         ret, th = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
         M = cv2.moments(th, True)
         row.extend(list(cv2.HuMoments(M).flatten()))
-        if 'suspect' in files[i]:
-            row.append(1)
-        else:
-            row.append(2)
-        writer.writerow(row)
+        if 'high' in files[i]:
+            writer.writerow(row)
+        if 'low' in files[i]:
+            writer1.writerow(row)
+        if 'severe' in files[i]:
+            writer2.writerow(row)
+        if 'normal' in files[i]:
+            writer3.writerow(row)
         row[:] = []
         i = i + 1
-    myFile.close()
 
+    myFile.close()
+    myFile1.close()
+    myFile2.close()
+    myFile3.close()
+
+
+def haralickMoments(files, images):
     # CODIGO PARA HARALICK
 
-    # Foi utilizada o pacote Mahotas para a extracao, esse pacote extrai 13 caracteristicas
-    # Como na definicao de Haralick, o calculo é feito em 4 direcoes distintas, na literatura é feito
-    # a media entre elas
-
-    myFile = open('CSVs\\Haralick' + specifier + '.csv', 'w', newline="")
     csv.register_dialect('dial', delimiter=';')
+    myFile = open('CSVs\\Haralick_High.csv', 'w', newline="")  # High file
     writer = csv.writer(myFile, dialect='dial')
+    myFile1 = open('CSVs\\Haralick_Low.csv', 'w', newline="")  # Low file
+    writer1 = csv.writer(myFile1, dialect='dial')
+    myFile2 = open('CSVs\\Haralick_Severe.csv', 'w', newline="")  # Lesao Grave file
+    writer2 = csv.writer(myFile2, dialect='dial')
+    myFile3 = open('CSVs\\Haralick_Normal.csv', 'w', newline="")  # Lesao Normal file
+    writer3 = csv.writer(myFile3, dialect='dial')
+
     row = []
     i = 0
     for img in images:
         row.append(files[i])  # Adiciona o nome da imagem no arquivo
-        # print(mahotas.features.haralick(img).mean(0))
         row.extend(list(mahotas.features.haralick(img).mean(0)))
-        if 'suspect' in files[i]:
-            row.append(1)
-        else:
-            row.append(2)
-        writer.writerow(row)
+        if 'high' in files[i]:
+            writer.writerow(row)
+        if 'low' in files[i]:
+            writer1.writerow(row)
+        if 'severe' in files[i]:
+            writer2.writerow(row)
+        if 'normal' in files[i]:
+            writer3.writerow(row)
         row[:] = []
         i = i + 1
+
     myFile.close()
+    myFile1.close()
+    myFile2.close()
+    myFile3.close()
 
-
+def lbp(files, images):
     # CODIGO PARA LBP
-
-    # A abordagem utilizada foi aplicar o LBP com alguns parametros pre-determinados
-    # o calculo foi feito gerando o histograma, essa é a abordagem feita na literatura
 
     lbp_sampling_points = 8  # Pontos tomados na vizinhança
     lbp_sampling_radius = 2  # Raio ao redor do pixel central
     method = "uniform"       # Metodo adotado para o LBP
     eps = 1e-7
 
-    myFile = open('CSVs\\LBP' + specifier + '.csv', 'w', newline="")
     csv.register_dialect('dial', delimiter=';')
+    myFile = open('CSVs\\LBP_High.csv', 'w', newline="")  # High file
     writer = csv.writer(myFile, dialect='dial')
+    myFile1 = open('CSVs\\LBP_Low.csv', 'w', newline="")  # Low file
+    writer1 = csv.writer(myFile1, dialect='dial')
+    myFile2 = open('CSVs\\LBP_Severe.csv', 'w', newline="")  # Lesao Grave file
+    writer2 = csv.writer(myFile2, dialect='dial')
+    myFile3 = open('CSVs\\LBP_Normal.csv', 'w', newline="")  # Lesao Normal file
+    writer3 = csv.writer(myFile3, dialect='dial')
+
     row = []
     i = 0
     for img in images:
@@ -110,21 +144,40 @@ def extractors(path):
         hist = hist.astype("float")
         hist /= (hist.sum() + eps)
         row = list(hist)
-        if 'suspect' in files[i]:
-            row.append(1)
-        else:
-            row.append(2)
-        writer.writerow(row)
+        if 'high' in files[i]:
+            writer.writerow(row)
+        if 'low' in files[i]:
+            writer1.writerow(row)
+        if 'severe' in files[i]:
+            writer2.writerow(row)
+        if 'normal' in files[i]:
+            writer3.writerow(row)
         row[:] = []
         i = i + 1
+
     myFile.close()
+    myFile1.close()
+    myFile2.close()
+    myFile3.close()
 
 
 def main():
+    files = []
     my_dirs = [d for d in os.listdir('output') if os.path.isdir(os.path.join('output', d))]
-    extractors(my_dirs[0])
     for dir in my_dirs:
-        extractors(dir)
+        # Descoberta de todas as imagens no diretorio passado
+        imdir = 'output\\' + dir + '\\'
+        # print(imdir)
+        ext = ['png', 'jpg']
+        [files.extend(glob.glob(imdir + '*.' + e)) for e in ext]
+
+    images = [cv2.imread(file, cv2.IMREAD_GRAYSCALE) for file in files]
+
+    statisticalMoments(files, images)
+    huMoments(files, images)
+    haralickMoments(files, images)
+    lbp(files, images)
+
     print("Finalizado com sucesso!")
 
 if __name__ == '__main__':
